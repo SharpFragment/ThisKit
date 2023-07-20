86b9a50dd2425da1c983b4f55ac7ec011256c94c
Sharp Fragment <fragment@sent.com>
2023-07-20 17:38:41 +0800

    force public

--- Sources/ThisKit/Logger.swift
+++ Sources/ThisKit/Logger.swift
@@ -31,43 +31,49 @@ public class TKLogger {
 //  }
 
   public func log(_ log: String,
+                  privacy: OSLogPrivacy = .public,
                   level: OSLogType = .default,
                   file: String = #file,
                   line: Int = #line,
                   column: Int = #column,
                   function: String = #function) {
-    logger.log(level: level, "\(file) \(function) \(line) \(column) \(log)")
+    // TODO
+    logger.log(level: level, "\(file) \(function) \(line) \(column) \(log, privacy: .public)")
   }
 
   public func debug(_ message: String,
+                    privacy: OSLogPrivacy = .public,
                     file: String = #file,
                     line: Int = #line,
                     column: Int = #column,
                     function: String = #function) {
-    log(message, level: .debug, file: file, line: line, column: column, function: function)
+    log(message, privacy: privacy, level: .debug, file: file, line: line, column: column, function: function)
   }
   
   public func info(_ message: String,
+                    privacy: OSLogPrivacy = .public,
                     file: String = #file,
                     line: Int = #line,
                     column: Int = #column,
                     function: String = #function) {
-    log(message, level: .info, file: file, line: line, column: column, function: function)
+    log(message, privacy: privacy, level: .info, file: file, line: line, column: column, function: function)
   }
   
   public func error(_ message: String,
+                    privacy: OSLogPrivacy = .public,
                     file: String = #file,
                     line: Int = #line,
                     column: Int = #column,
                     function: String = #function) {
-    log(message, level: .error, file: file, line: line, column: column, function: function)
+    log(message, privacy: privacy, level: .error, file: file, line: line, column: column, function: function)
   }
   
   public func fault(_ message: String,
+                    privacy: OSLogPrivacy = .public,
                     file: String = #file,
                     line: Int = #line,
                     column: Int = #column,
                     function: String = #function) {
-    log(message, level: .fault, file: file, line: line, column: column, function: function)
+    log(message, privacy: privacy, level: .fault, file: file, line: line, column: column, function: function)
   }
 }
